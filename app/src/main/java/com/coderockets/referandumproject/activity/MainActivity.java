3a256d9822645b773b3f87efc05c69ecdcbfd78b
Aykut Asil <aykuttasil@gmail.com>
2016-06-02 17:31:52 +0300

    Add Toolbar

--- app/src/main/java/com/coderockets/referandumproject/activity/MainActivity.java
+++ app/src/main/java/com/coderockets/referandumproject/activity/MainActivity.java
@@ -1,22 +1,36 @@
 package com.coderockets.referandumproject.activity;
 
 
+import android.support.v7.widget.Toolbar;
+
 import com.coderockets.androidhelper.helper.UiHelper;
 import com.coderockets.referandumproject.R;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import hugo.weaving.DebugLog;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
+    @ViewById(R.id.toolbar)
+    Toolbar mToolbar;
 
     @DebugLog
     @AfterViews
     public void MainActivityInit() {
-        UiHelper.UiDialog.newInstance(this).getOKDialog("Deneme Title", "Deneme Content", null).show();
+        //UiHelper.UiDialog.newInstance(this).getOKDialog("Deneme Title", "Deneme Content", null).show();
+        setToolbar();
     }
+
+    @DebugLog
+    private void setToolbar() {
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("REFERANDUM");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
 
 }
