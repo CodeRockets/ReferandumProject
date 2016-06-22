1524ae5f67cf281d777c63338b1277756e8d8f56
Aykut Asil <aykuttasil@gmail.com>
2016-06-22 15:27:19 +0300

    Add Fonts

--- app/src/main/java/com/coderockets/referandumproject/activity/BaseActivity.java
+++ app/src/main/java/com/coderockets/referandumproject/activity/BaseActivity.java
@@ -1,9 +1,17 @@
 package com.coderockets.referandumproject.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 /**
  * Created by aykutasil on 2.06.2016.
  */
 public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 }
